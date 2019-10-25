3535238b87502cdebc24dd1bb5acd57b9b87f383
Arseny Malkov <arsenymalkov@gmail.com>
2019-10-25 19:00:02 +1300

    Add get and update dream in MainViewModel

--- app/src/main/java/com/dreamdiary/ui/main/MainViewModel.kt
+++ app/src/main/java/com/dreamdiary/ui/main/MainViewModel.kt
@@ -1,7 +1,19 @@
 package com.dreamdiary.ui.main
 
 import androidx.lifecycle.ViewModel
+import com.dreamdiary.persistance.Dream
+import com.dreamdiary.persistance.DreamDao
+import io.reactivex.Completable
+import io.reactivex.Flowable
 
-class MainViewModel : ViewModel() {
-    // TODO: Implement the ViewModel
+class MainViewModel(private val dataSource: DreamDao) : ViewModel() {
+
+    fun getDream(id: String): Flowable<Dream> {
+        return dataSource.getDreamById(id)
+    }
+
+    fun updateDream(dream: Dream): Completable {
+        return dataSource.insertDream(dream)
+    }
+
 }
