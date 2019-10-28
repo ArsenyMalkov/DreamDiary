e2436828ddf9cae908de76dfec187b64ce731991
Arseny Malkov <arsenymalkov@gmail.com>
2019-10-29 00:16:09 +1300

    Add dream list to MainViewModel

--- app/src/main/java/com/dreamdiary/ui/main/MainViewModel.kt
+++ app/src/main/java/com/dreamdiary/ui/main/MainViewModel.kt
@@ -1,12 +1,17 @@
 package com.dreamdiary.ui.main
 
 import androidx.lifecycle.ViewModel
+import androidx.paging.PagedList
+import androidx.paging.toObservable
 import com.dreamdiary.persistance.Dream
 import com.dreamdiary.persistance.DreamDao
 import io.reactivex.Completable
 import io.reactivex.Flowable
+import io.reactivex.Observable
 
 class MainViewModel(private val dataSource: DreamDao) : ViewModel() {
+
+    val dreamList: Observable<PagedList<Dream>> = dataSource.getDreams().toObservable(pageSize = 50)
 
     fun getDream(id: String): Flowable<Dream> {
         return dataSource.getDreamById(id)
