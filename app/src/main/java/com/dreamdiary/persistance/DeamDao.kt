76fd4dd889732de94e67958fc2a5278342dece6b
Arseny Malkov <arsenymalkov@gmail.com>
2019-10-29 00:13:56 +1300

    Add getDreams method to DreamDao

--- app/src/main/java/com/dreamdiary/persistance/DeamDao.kt
+++ app/src/main/java/com/dreamdiary/persistance/DeamDao.kt
@@ -1,5 +1,6 @@
 package com.dreamdiary.persistance
 
+import androidx.paging.DataSource
 import androidx.room.Dao
 import androidx.room.Insert
 import androidx.room.OnConflictStrategy
@@ -9,6 +10,9 @@ import io.reactivex.Flowable
 
 @Dao
 interface DreamDao {
+
+    @Query("SELECT * FROM dreams")
+    fun getDreams(): DataSource.Factory<Int, Dream>
 
     @Query("SELECT * FROM dreams where id = :id")
     fun getDreamById(id: String): Flowable<Dream>
