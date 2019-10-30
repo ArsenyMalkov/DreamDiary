8357b4469ab162b75879116289e9365cb9e447a9
Arseny Malkov <arsenymalkov@gmail.com>
2019-10-30 17:03:21 +1300

    Set export schema to false

--- app/src/main/java/com/dreamdiary/persistance/DreamsDatabase.kt
+++ app/src/main/java/com/dreamdiary/persistance/DreamsDatabase.kt
@@ -5,7 +5,7 @@ import androidx.room.Database
 import androidx.room.Room
 import androidx.room.RoomDatabase
 
-@Database(entities = [Dream::class], version = 1)
+@Database(entities = [Dream::class], version = 1, exportSchema = false)
 abstract class DreamsDatabase : RoomDatabase() {
 
     abstract fun dreamDao() : DreamDao
